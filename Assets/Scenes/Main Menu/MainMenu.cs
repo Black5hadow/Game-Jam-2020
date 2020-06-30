287b00b9956ed43a494c55c0a4632e71dd32e048
Black5hadow <64100152+Black5hadow@users.noreply.github.com>
2020-06-30 17:48:21 -0400

    0.1

---
+++ Assets/Scenes/Main Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Starts Game
+    
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
